0331eb2275ab2d58ae81b73afcb1576d778405f1
Yves Manzi <yv.manzi@gmail.com>
2023-03-28 10:03:48 +0200

    [ADD] Project setup

---
+++ src/main/java/com/switchfully/eurder/EurderApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.eurder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/switchfully/eurder/EurderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfully.eurder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
